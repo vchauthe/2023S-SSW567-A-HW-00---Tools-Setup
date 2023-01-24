85aa4028ab218a6935cded9be20617b4b682a33d
vchauthe <120316181+vchauthe@users.noreply.github.com>
2023-01-24 18:29:02 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import datetime
+
+def my_brand(assignment_name):
+    print("=*=*=*= Vaibhav Chauthe =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-A =*=*=*=")
+    print("=*=*=*= The name of the homework assignment =*=*=*= " + assignment_name)
+    print("=*=*=*= The date and time at which the function was called =*=*=*= " + str(datetime.datetime.now()))
+
+
+my_brand("HW 00 - Tools Setup")
+print("Hello world")
+my_brand("HW 00 - Tools Setup")
+
